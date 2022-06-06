1b5de642216fde147f4b8261bbfea1913f33c5d8
Hiroshi SUGIMURA <hiroshi.sugimura@gmail.com>
2022-06-06 17:22:58 +0900

    omron

--- v1/src/main.js
+++ v1/src/main.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 
 //////////////////////////////////////////////////////////////////////
 // 基本設定，electronのファイル読み込み対策，developmentで変更できるようにした（けどつかってない）
-const appname  = 'HEMS-Logger';
+const appname  = 'OmronEnvStore';
 const appDir   = process.env.NODE_ENV === 'development' ? __dirname : __dirname;
 const isWin    = process.platform == "win32" ? true : false;
 const isMac    = process.platform == "darwin" ? true : false;
@@ -30,40 +30,16 @@ process.env["NODE_CONFIG_DIR"] = configDir; // コンフィグファイル置き
 //////////////////////////////////////////////////////////////////////
 // 追加ライブラリ
 const { app, BrowserWindow, ipcMain, Menu, shell } = require('electron');
-const { sqlite3, elrawModel, eldataModel, esmdataModel, esmrawModel, huerawModel, arpModel, owmModel, netatmoModel, IOT_QuestionnaireAnswersModel, IOT_MajorResultsModel, IOT_MinorResultsModel, IOT_MinorkeyMeansModel, MinorkeyMeansValues } = require('./models/localDBModels');   // DBデータと連携
+const { sqlite3 } = require('./models/localDBModels');   // DBデータと連携
 const { Op } = require("sequelize");
-const mainArp     = require('./mainArp');     // arpの管理
-const mainEL      = require('./mainEL');      // ELの管理
-const mainESM     = require('./mainESM'); // スマートメータの管理
-const mainHue     = require('./mainHue');     // hueの管理
-// const mainIkea    = require('./mainIkea');    // Ikeaの管理
-const mainNetatmo = require('./mainNetatmo'); // netatmoの管理
-const mainOwm     = require('./mainOwm');     // open weather mapの管理
-const mainHALlocal = require('./mainHALlocal');    // HAL，独立で動く部分
-const mainHALsync  = require('./mainHALsync');    // HAL，連携する部分
+
+const omron = require('usb-2jcie-bu');
+const cron = require('node-cron');
+require('date-utils');
 
 
 // electronのmain window
 let mainWindow = null;
-
-
-// 管理しているデバイスやサービスのリストにユーザが名前を付けたい
-// [{ type: '', id: '', ip: '', mac: '', alias, '' }]
-let managedThings = [];
-
-// 監視しているデバイスのリスト
-// [{id:, obj, epc}]
-let elObservedDevs = [];
-
-// 管理しているデバイスやサービスのデータ詳細
-let elData = {};
-let arpTable = [];
-let hueData = {};
-let owmData = {};
-let netatmoData = {};
-// let ikeaData = {};
-let halProfile = {};
-let halData = {};
 
 
 //////////////////////////////////////////////////////////////////////
@@ -72,37 +48,6 @@ let halData = {};
 // configファイルがなければ作る，あれば読む
 // デフォルトの値にmargeする
 let config = {
-	"height": "",
-	"weight": "",
-	"ellogExpireDays": 30,
-	"resultExpireDays": 365,
-	"network": {
-		"IPver": 0,
-		"IPv4": "",
-		"IPv6": ""
-	},
-	"hueKey": "",
-	"owmAPIKey": "",
-	"zipCode": "",
-	"netatmo": {
-		"id": "",
-		"secret": "",
-		"username": "",
-		"password": ""
-	},
-	"ESM": {
-		"enable": 'false',
-		"donglePass": '',
-		"donglePassCandidates": [],
-		"dongleType": '',
-		"id":'',
-		"password":'',
-		"EPANDESC":{},
-		"connected": false,
-		"debug": false
-	},
-	"observationInterval": 0,
-	"observationDevs": ''
 };
 
 
@@ -164,14 +109,7 @@ console.log('ipver:', config.network.IPver, 'ipv4:', config.network.IPv4, 'ipv6:
 
 // デフォルトの値にmargeする
 let persist = {
-	elData: {},
-	elObservedDevs: [],
-	arpTable: [],
-	hueData: {},
-	owmData: {},
-	netatmoData: {},
-	halProfile: {},
-	halData: {}
+
 };
 
 // 終了時の状態の保存
@@ -208,13 +146,6 @@ let loadPersistFile = function () {
 		});
 	}
 	elData      = persist.elData || {};
-	elObservedDevs = persist.elObservedDevs || [];
-	arpTable    = persist.arpTable || [];
-	hueData     = persist.hueData || {};
-	owmData     = persist.owmData || {};
-	netatmoData = persist.netatmoData || {};
-	halProfile  = persist.halProfile || {};
-	halData     = persist.halData || {};
 };
 
 // 起動時に一回readしておく
@@ -228,6 +159,7 @@ loadPersistFile();
 		config.ESM.donglePassCandidates.push(p.path);
 	});
 })();
+
 
 //////////////////////////////////////////////////////////////////////
 // local function
@@ -279,166 +211,19 @@ let getToday = function() {
 
 
 //////////////////////////////////////////////////////////////////////
-// EL管理
-let ELStart = function() {
-	// mainEL初期設定
-	mainEL.observationDevs = elObservedDevs;
-
-	mainEL.start(config.network,
-				 (rinfo, els, err) => {  // els received, 受信のたびに呼ばれる
-					 // database
-					 // 確認
-					 let rawdata = mainEL.api.getSeparatedString_ELDATA(els);
-
-					 mainEL.conv.elsAnarysis(els, function( eljson ) {
-						 // console.log("----");
-						 for (const [key, value] of Object.entries(eljson.EDT) )
-						 {
-							 eldataModel.create({ srcip: rinfo.address, srcmac:mainArp.toMAC(rinfo.address), seoj: eljson.SEOJ, deoj: eljson.DEOJ, esv: eljson.ESV, epc: key, edt: value });
-						 }
-					 } );
-					 elrawModel.create({ srcip: rinfo.address, srcmac:mainArp.toMAC(rinfo.address), dstip:localaddresses[0], dstmac:mainArp.toMAC(localaddresses[0]), rawdata: rawdata, seoj: els.SEOJ, deoj: els.DEOJ, esv: els.ESV, opc: els.OPC, detail: els.DETAIL });
-				 },
-				 (facilities) => {  // change facilities, 全体監視して変更があったときに全体データとして呼ばれる
-					 elData = facilities;
-					 elObservedDevs = mainEL.observationDevs;
-					 mainEL.conv.refer( objectSort(facilities) , function (devs) {
-						 sendIPCMessage( "fclEL", objectSort(devs) );
-					 });
-				 });
-};
+// Omron管理
+omron.start(  (sensorData) => {
+	console.log( '----------------------------' );
+	let dt = new Date();
+	console.log( dt );
+	console.dir( sensorData );
+});
 
 
-//////////////////////////////////////////////////////////////////////
-// スマートメータ管理
-let ESMStart = function() {
-	if( config.ESM.enable == 'false' ) return;
-	if( config.ESM.donglePass == '' ) return;
-
-	console.log('-- ESMStart');
-	console.dir( config.ESM.debug = true );
-
-	mainESM.start( config.ESM,
-				   ( sm, rinfo, els, err ) => {
-					   if( err ) {
-						   sendIPCMessage( "ERR", '' + err + '<br>スマートメータの設定をもう一度確認し、一度アプリを再起動してみてください。' );
-						   return;
-					   }
-					   if( !els ) return;  // elsが入っていないときは処理しない
-
-					   console.dir( els );
-					   config.ESM.EPANDESC = mainESM.config.EPANDESC;  // 接続できたので接続情報を確保
-					   config.ESM.connected = true;  // 接続できたフラグ
-
-					   let rawdata = mainEL.api.getSeparatedString_ELDATA(els);
-					   mainEL.conv.elsAnarysis(els, function( eljson ) {
-						   for (const [key, value] of Object.entries(eljson.EDT) )
-						   {
-							   esmdataModel.create({ srcip: rinfo.address, seoj: eljson.SEOJ, deoj: eljson.DEOJ, esv: eljson.ESV, epc: key, edt: value });
-						   }
-						   config.ESM.connected = true;
-					   });
-					   esmrawModel.create({ srcip: rinfo.address, rawdata: rawdata, seoj: els.SEOJ, deoj: els.DEOJ, esv: els.ESV, opc: els.OPC, detail: els.DETAIL });
-				   },
-				   (facilities) => {
-					   mainEL.conv.refer( objectSort(facilities) , function (devs) {
-						   // console.dir( objectSort(devs) );
-						   sendIPCMessage( "fclESM", objectSort(devs) );
-					   });
-				   });
-
-};
-
-//////////////////////////////////////////////////////////////////////
-// arp管理
-let ArpStart = function () {
-	if( config.owmAPIKey != '' ) {
-		mainArp.start( (table) => {
-			arpTable = table;
-			arpModel.create({ detail: JSON.stringify(arpTable) });
-		});
-	}
-};
-
-
-//////////////////////////////////////////////////////////////////////
-// hue管理
-
-let HueStart = function () {
-	// keyがあればHue利用開始，なければ実行しない。
-	if( config.hueKey == undefined && config.hueKey == '' ) {
-		console.log('-- Skipped, HueStart() key:', config.hueKey);
-		return;
-	}
-
-	mainHue.start( config.hueKey, 3000,
-				   (key) => {  // Linked callback
-					   config.hueKey = key;
-					   sendIPCMessage( "HueLinked", key );
-				   },
-				   (json) => {  // changed callback
-					   if( json != '' ) {
-						   hueData = JSON.parse(json);
-						   sendIPCMessage( "fclHue", hueData );
-						   huerawModel.create({ detail: json });
-					   }
-				   });
-};
-
-
-//////////////////////////////////////////////////////////////////////
-// OpenWeatherMapの処理
-
-let OwmStart = function () {
-	if( config.owmAPIKey != '' ) {
-		mainOwm.start( config.owmAPIKey, config.zipCode, (body) => {
-			owmData = body;
-			// console.log('owmData:', owmData);
-			if( owmData != {} ) {
-				sendIPCMessage( "newOwm", owmData );
-				owmModel.create({ detail: JSON.stringify(owmData) }); // dbに入れる
-			}
-
-			sendIPCMessage( "newOwm", owmData );
-			owmModel.create({ detail: JSON.stringify(owmData) });// dbに入れる
-		});
-	}
-};
-
-
-//////////////////////////////////////////////////////////////////////
-// netatmo start
-
-let NetatmoStart = function () {
-	if( config.netatmo.id != '' && config.netatmo.secret != '' && config.netatmo.username != '' && config.netatmo.password != '') {// configがあればnetatmo利用開始，なければ実行しない。
-		mainNetatmo.start( config.netatmo, (err, devices) => {
-			if(err) {
-				console.error(err);
-				return;
-			}
-
-			netatmoData = devices;
-			sendIPCMessage( "newNetatmo", netatmoData );
-			netatmoModel.create({ detail: JSON.stringify(netatmoData) });// dbに入れる
-		});
-	}
-};
-
-
-
-//////////////////////////////////////////////////////////////////////
-// ikea start
-/*
-let IkeaStart = function () {
-	mainIkea.start( key, identity, psk, ( facilities ) => {
-		if( mainWindow != null ) {
-			if( mainWindow.webContents != null ) {
-				sendIPCMessage( "fclIKEA", facilities  );
-			}
-		}
-	} );
-};
-*/
+// 2秒毎にチェック
+cron.schedule('*/2 * * * * *', () => {
+	omron.requestData();
+});
 
 
 
@@ -473,91 +258,6 @@ ipcMain.on('to-main', function (event, arg) {
 		break;
 
 		//----------------------------------
-		// EL操作
-		case 'Search':
-		mainEL.api.search();
-		break;
-
-		case 'Elsend':   // Rendererからの ECHONET Liteコントロール
-		// 送信は自分のログも残しておく
-		let datetime = new Date().toISOString();
-		let rawdata = c.arg.sendmsg;
-		let els = mainEL.api.parseString( c.arg.sendmsg );
-
-		mainEL.conv.elsAnarysis(els, function( eljson ) {
-			for (const [key, value] of Object.entries(eljson.EDT) )
-			{
-				eldataModel.create({ srcip: localaddresses[0], srcmac:mainArp.toMAC(localaddresses[0]), seoj: eljson.SEOJ, deoj: eljson.DEOJ, esv: eljson.ESV, epc: key, edt: value });
-			}
-		});
-		elrawModel.create({ srcip: localaddresses[0], srcmac:mainArp.toMAC(localaddresses[0]), dstip: c.arg.ip, dstmac:mainArp.toMAC(c.arg.ip), rawdata: rawdata, seoj: els.SEOJ, deoj: els.DEOJ, esv: els.ESV, opc: els.OPC, detail: els.DETAIL });
-		mainEL.api.sendString( c.arg.ip, c.arg.sendmsg );
-		break;
-
-
-		//----------------------------------
-		case 'ESMUse':   // スマートメータ利用開始
-		console.log('esm start:', c.arg);
-		// 他の設定もあるので config.ESM = c.arg への置き換えは不可
-		config.ESM.enable = true;
-		config.ESM.donglePass = c.arg.donglePass;
-		config.ESM.dongleType = c.arg.dongleType;
-		config.ESM.id = c.arg.id;
-		config.ESM.password = c.arg.password;
-		config.ESM.connected = false;  // 再設定のために，接続経験なしにする
-		config.ESM.EPANDESC = {};  // 再設定のために，接続経験なしにする
-		ESMStart();
-		writeConfigFile();
-		break;
-
-		case 'ESMnotUse':   // スマートメータ利用停止
-		config.ESM.enable = false;
-		mainESM.release();
-		writeConfigFile();
-		break;
-
-		//----------------------------------
-		case 'Huesend':  // Hue関係のコントロール
-		mainHue.api.setState( mainHue.api.bridge.ipaddress, c.arg.url, c.arg.json );
-		break;
-
-		//----------------------------------
-		case 'HueUse':   // Hue利用開始
-		console.log('hue start:', c.arg.key);
-		HueStart();
-		break;
-
-		//----------------------------------
-		case 'OwmUse':// OpenWeatherMap利用開始
-		console.log('OpenWeatherMap start:', c.arg.owmAPIKey, 'zipCode:', c.arg.zipCode);
-		config.owmAPIKey = c.arg.owmAPIKey;
-		config.zipCode = c.arg.zipCode;
-		OwmStart();
-		break;
-
-		//----------------------------------
-		case 'NetatmoUse':// OpenWeatherMap利用開始
-		console.log('Netatmo start:');
-		console.dir(c.arg);
-		config.netatmo = c.arg.netatmo;
-		NetatmoStart();
-		break;
-
-		//----------------------------------
-		case 'renewHAL':// 最新のHAL要求受付
-		console.log('renewHAL:');
-		halData = mainHALlocal.getLastData();
-		sendIPCMessage( "renewHAL", halData );
-		break;
-
-		case "submitQuestionnaire": // HALアンケートが来た
-		console.log('submitQuestionnaire:');
-		mainHALlocal.submitQuestionnaire(c.arg,
-										 () => { sendIPCMessage( 'INF', 'アンケートを保存しました。' ); },
-										 () => { sendIPCMessage( 'INF', 'Error: ' + error.message    ); });
-		break;
-
-		//----------------------------------
 		// 設定保存
 		case 'configSave':
 		console.log('configSave start:');
@@ -566,52 +266,6 @@ ipcMain.on('to-main', function (event, arg) {
 		config.ellogExpireDays = c.arg.ellogExpireDays;
 		config.resultExpireDays = c.arg.resultExpireDays;
 		writeConfigFile();
-		break;
-
-		//----------------------------------
-		// 同期開始
-		case 'Sync':
-		console.log('Sync start:');
-		mainHALsync.startSync( (result) => {
-			sendIPCMessage( "Synced", result );
-		});
-		break;
-
-		//----------------------------------
-		// ローカルの HAL API トークン取得
-		case 'getHalApiTokenRequest':
-		console.log('getHalApiTokenRequest start:');
-		sendIPCMessage( "getHalApiTokenResponse", mainHALsync.halApiToken );
-		break;
-
-		//----------------------------------
-		// HAL API トークン設定
-		case 'setHalApiTokenRequest':
-		console.log('setHalApiTokenRequest start:');
-		mainHALsync.setHalApiTokenRequest( c.arg, (result) => {
-			sendIPCMessage( "setHalApiTokenResponse", result );
-		});
-		break;
-
-		//----------------------------------
-		// HAL API トークン設定削除
-		case 'deleteHalApiToken':
-		console.log('deleteHalApiToken:');
-		mainHALsync.deleteHalApiToken( () => {
-			sendIPCMessage( "deleteHalApiTokenResponse", null );
-			halProfile = mainHALsync.halProfile;
-		});
-		break;
-
-
-		//----------------------------------
-		// HAL ユーザープロファイル取得
-		case 'getHalUserProfileRequest':
-		console.log('getHalUserProfileRequest start:');
-		mainHALsync.getHalUserProfileRequest( (result) => {
-			sendIPCMessage( "getHalUserProfileResponse", result );
-			halProfile = mainHALsync.halProfile;
-		});
 		break;
 
 		default:
@@ -727,7 +381,6 @@ const menuItems = [{
 		}
 		]
 }];
-
 
 
 function menuInitialize() {
